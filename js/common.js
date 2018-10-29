39cd281429a5bea5525b38172c71dba717a5156f
Vlad Filinsky <vfilinsky.sells@gmail.com>
2018-10-29 10:38:17 +0500

    блок ком.недвижимость

--- js/common.js
+++ js/common.js
@@ -2,17 +2,43 @@ $(function() {
 
 $('.benefit-block-box_list > li').prepend('<i class="fa fa-dot-circle-o"></i>');
 
+  if ($(window).width() < 1196) { //1169
+    $(".wrapper > .container").removeClass("container").addClass("container-fluid");
+ }
+ else if($(window).width() > 1196) { // 1169
+     $(".wrapper > .container-fluid").removeClass("container-fluid").addClass("container");
+ }
+
 $("* #phone").mask("+7(000)000-00-00", {
     placeholder: "+7(___)___-__-__",
     clearIfNotMatch: true
   });
 
-$(".owl-carousel").owlCarousel({
+$(".park").owlCarousel({
     nav:true,
     animateOut: 'fadeOut',
     dots: false,
     navText: false,
     autoHeight:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        1000:{
+            items:1
+        }
+    }
+});
+$(".realtyinfo-slider").owlCarousel({
+    dots: false,
+    navText : ['<i class="fa fa-angle-left">','<i class="fa fa-angle-right">'],
+    autoHeight: true,
+    center: true,
+    smartSpeed:500,
+    nav: true,
     responsive:{
         0:{
             items:1
